cf0d80b24477e3416dc56fde6e42201d16362021
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-24 09:46:34 +0100

    added translation

--- www/app/main.js
+++ www/app/main.js
@@ -10,13 +10,21 @@ export let mainModule = angular.module('famous-sample', [
 
 mainModule.controller('MainCtrl', ['$scope', '$famous', '$timeout', function($scope, $famous, $timeout){
 	var Transitionable = $famous['famous/transitions/Transitionable'];
+	var Easing = $famous['famous/transitions/Easing'];
 	var t = new Transitionable(0);
+
+
+	$scope.myTransitionable = new Transitionable([0, 0, 0]);
+	//$scope.myTransitionable.set([100, 0, 0], {duration: 1000, curve: 'easeInOut'})
 
 	function reset(){
 		t.set(0);
-		t.set(Math.PI * 2.0, {duration: 1750, curve: 'linear'}, function(){
-			$timeout(reset);
-		})
+		t.set(Math.PI * 2.0, {duration: 3000, curve: 'linear'}, function(){
+			//$timeout(reset);
+		});
+		$scope.myTransitionable.set([300, 0, 0], {duration: 3000, curve: Easing.outBounce}, function(){
+			//$timeout(reset);
+		});
 	}
 
 	reset();
